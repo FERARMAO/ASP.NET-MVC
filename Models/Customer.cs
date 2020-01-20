d21e5e6de10d2d147cc1357c71410d4baed06058
Oussama Ferarma <57950740+FERARMAO@users.noreply.github.com>
2020-01-20 16:39:43 +0100

    Update Customer.cs

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -10,19 +10,19 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
        
-        [Required] //with this annotation above name our column name will not be null. but in VS2019 elle l'est plus déjà mais je laisse ça qd mm.
+        [Required] 
         [StringLength(255)]
         public string Name { get; set; }
        
         public bool IsSubscribedToNewsletter { get; set; }
         
-        public MembershipType MembershipType { get; set; } //Ps: MembershipType (la 2éme) is called navigation property, because it allows us to navigate from one type to another (in this case from Customer to its MembershipType).
+        public MembershipType MembershipType { get; set; } 
         
         [Display(Name = "Membership Type")]
-        public byte MembershipTypeId { get; set; } //EF reconize this convention and will treat this property as a foreign key.
+        public byte MembershipTypeId { get; set; } 
        
         [Display(Name= "Date of Birth")]
         [Min18YearsIfAMember]
-        public DateTime? Birthdate { get; set; } //The ? means it's optional (nullable variable).
+        public DateTime? Birthdate { get; set; } 
     }
 }
