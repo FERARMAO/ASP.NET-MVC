67b9fa634a098554733a1644fd987d2bba6c6227
Oussama Ferarma <57950740+FERARMAO@users.noreply.github.com>
2020-01-20 16:40:24 +0100

    Update MembershipType.cs

--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -8,7 +8,7 @@ namespace Vidly.Models
 {
     public class  MembershipType 
     {
-        public byte Id { get; set; } //Ps: we're using byte because we don't have a big number...
+        public byte Id { get; set; } 
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
