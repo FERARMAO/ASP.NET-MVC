02023ae414e9e1d8ca0922d752c997159cc72177
Oussama Ferarma <57950740+FERARMAO@users.noreply.github.com>
2020-01-20 16:43:06 +0100

    Update CustomerFormViewModel.cs

--- ViewModels/CustomerFormViewModel.cs
+++ ViewModels/CustomerFormViewModel.cs
@@ -6,7 +6,7 @@ using Vidly.Models;
 
 namespace Vidly.ViewModels
 {
-    public class CustomerFormViewModel //dans cette class, on hérite que des propriété de Customer et MembershipType classes, mais pas de leur contenu !!! TRES IMPORTANT. Le contenu c'est dans le Controller qu'on le déclare avec ActionResult(que ce soit de la BD ou par hard coding).
+    public class CustomerFormViewModel 
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }
